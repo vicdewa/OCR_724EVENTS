803e58af348c9cf217f8ec6c3e6aed960a7b4c04
Victoire Dewaegeneire <victoire.dew@gmail.com>
2025-01-25 17:54:52 +0100

    Commit portfolio

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -34,7 +34,7 @@ const Slider = () => {
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={event.cover} alt="forum" loading="lazy"/>
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
